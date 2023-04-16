a274efa4d3045a2d447116c706e748bd3ca4062d
kaipol <1335791724@qq.com>
2023-04-16 12:46:30 +0800

    Site updated: 2023-04-16 12:46:30

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","9910cae61156d5b805b512404c9d1e18"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
